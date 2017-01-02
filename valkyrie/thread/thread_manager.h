766ca8c658be2fda155a08d66028c5f1288038a9
bachelor.whc@gmail.com <bachelor.whc@gmail.com>
2017-01-02 21:19:17 +0800

    ThreadManager added.

---
+++ valkyrie/thread/thread_manager.h
@@ -0,0 +1,16 @@
+#ifndef _VALKYRIE_THREAD_MANAGER_H 
+#define _VALKYRIE_THREAD_MANAGER_H 
+#include "common.h"
+
+namespace Valkyrie {
+	class ThreadManager {
+	public:
+		ThreadManager();
+		virtual ~ThreadManager();
+
+	private:
+
+	};
+}
+
+#endif
