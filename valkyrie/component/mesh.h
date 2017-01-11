638b0067b6a61dd3fd7a643bac4f430dad3f2108
bachelor.whc@gmail.com <bachelor.whc@gmail.com>
2017-01-11 18:03:36 +0800

    Mesh component.

--- valkyrie/component/mesh.h
+++ valkyrie/component/mesh.h
@@ -1,15 +1,29 @@
 #ifndef _VALKYRIECOMPONENT_MESH_H
 #define _VALKYRIECOMPONENT_MESH_H
 #include "valkyrie/component/component.h"
+#include "valkyrie/vulkan/memory_buffer.h"
 
 namespace ValkyrieComponent {
+	class MeshSupport {
+	public:
+		MeshSupport() {};
+		virtual ~MeshSupport() {};
+
+	private:
+		
+	};
+
+	using MeshSupportPtr = std::shared_ptr<MeshSupport>;
+
 	class Mesh : public Component {
 	public:
-		Mesh() {};
+		Mesh() = delete;
+		Mesh(const MeshSupportPtr& mesh_ptr);
 		virtual ~Mesh() {};
 
 	private:
-
+		Vulkan::MemoryBuffer m_vulkan_buffer;
+		MeshSupportPtr m_mesh_support_ptr;
 	};
 }
 
