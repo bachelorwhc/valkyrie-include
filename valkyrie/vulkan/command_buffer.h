67c72c6c029856c3b1af60030a27c9c2803ed0c2
bachelor.whc@gmail.com <bachelor.whc@gmail.com>
2017-06-10 20:00:47 +0800

    add beginInformation to command buffer for settings of secondary buffer.

--- valkyrie/vulkan/command_buffer.h
+++ valkyrie/vulkan/command_buffer.h
@@ -8,6 +8,12 @@ namespace Vulkan {
 
 	struct CommandBuffer {
 		VkCommandBuffer handle = VK_NULL_HANDLE;
+		VkCommandBufferBeginInfo beginInformation = {
+			VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
+			nullptr,
+			NULL,
+			nullptr
+		};
 		VkResult begin();
 		VkResult end();
 		VkResult submit(const Queue& queue);
