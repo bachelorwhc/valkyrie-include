49dcba7644876a1e6d86b8442a82a85db1ba47c9
bachelor.whc@gmail.com <bachelor.whc@gmail.com>
2017-06-18 03:04:30 +0800

    bounding_box.

---
+++ valkyrie/scene/bounding_box.h
@@ -0,0 +1,22 @@
+#ifndef _BOOBS_ROUNDING_BOX_H_
+#define _BOOBS_ROUNDING_BOX_H_
+#include <glm/glm.hpp>
+
+namespace Valkyrie {
+	class RoundingBox {
+	public:
+		RoundingBox();
+		RoundingBox(const glm::vec3& position, const glm::vec3& diagonal);
+		virtual ~RoundingBox();
+
+		bool overlap(const RoundingBox& rhs) const;
+
+		glm::vec3 min;
+		glm::vec3 max;
+	};
+}
+
+Valkyrie::RoundingBox operator&(const Valkyrie::RoundingBox& lhs, const Valkyrie::RoundingBox& rhs);
+Valkyrie::RoundingBox operator|(const Valkyrie::RoundingBox& lhs, const Valkyrie::RoundingBox& rhs);
+
+#endif
