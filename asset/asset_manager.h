80672b582f71040c1e943f68dc42198810f76bed
bachelor.whc@gmail.com <bachelor.whc@gmail.com>
2017-06-21 19:35:20 +0800

    texture

--- asset/asset_manager.h
+++ asset/asset_manager.h
@@ -4,6 +4,7 @@
 #include <unordered_map>
 #include "valkyrie/asset/asset.h"
 #include "valkyrie/asset/lavy_loader.h"
+#include "valkyrie/asset/image.h"
 
 namespace Valkyrie {
 	class MemoryChunk;
@@ -20,18 +21,22 @@ namespace Valkyrie {
 
 		void load(std::experimental::filesystem::path file_path) throw(...);
 		void fillMemoryFromFile(MemoryChunkPtr& ptr, const std::experimental::filesystem::path& abs_path) throw(...);
-		std::experimental::filesystem::path getAssetsDirectory() { return m_path; };
-		AssetPtr getAsset(std::string relative_path);
-		MeshPtr getMesh(std::string mesh_name);
+		std::experimental::filesystem::path getAssetsDirectory() const { return m_path; };
+		AssetPtr getAsset(const std::string& relative_path);
+		MeshPtr getMesh(const std::string& mesh_name);
+		ImageMemoryPointer getImage(const std::string& relative_path);
 
 	private:
 		static AssetManager* gp_asset_manager;
 		AssetManager();
 
+		ImageMemoryPointer loadImage(const std::string file_path);
+
 		std::experimental::filesystem::path m_path;
 		std::unordered_map <std::string, AssetPtr> m_asset_map;
 		std::unordered_map <std::string, MeshPtr> m_mesh_map;
 		std::unordered_map <std::string, MemoryChunkPtr> m_memory_chunk_map;
+		std::unordered_map <std::string, ImageMemoryPointer> m_image_map;
 		LavyLoader m_lavy_loader;
 	};
 }
