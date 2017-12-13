810367c1f5c1210a43e9f7e21da86e98678dd662
Franklin Wang <franklinwang@reallusion.com>
2017-12-13 10:14:52 +0800

    add light supported.

--- scene/light_shader_writer.h
+++ scene/light_shader_writer.h
@@ -12,18 +12,15 @@ namespace Valkyrie {
         bool addLight(unsigned int ID);
         void removeLight(unsigned int ID);
         void updateLights();
-        VkWriteDescriptorSet* getPositionLightWrites();
+        VkWriteDescriptorSet getPositionLightWrite();
         uint32_t getPositionLightWritesCount() const;
         Vulkan::MemoryBuffer getPositionLightCountBuffer() const;
-        VkWriteDescriptorSet* getDummyPositionLightWrites();
-        uint32_t getDummyPositionLightWritesCount() const;
 
     private:
         std::vector<unsigned int> m_position_lights;
-        std::unordered_map<unsigned int, Vulkan::MemoryBuffer> m_position_light_buffer;
-        std::vector<VkWriteDescriptorSet> m_position_light_writes;
+        Vulkan::MemoryBuffer m_position_lights_buffer;
         Vulkan::MemoryBuffer m_position_light_count_buffer;
-        std::vector<VkWriteDescriptorSet> m_dummy_position_light_writes;
-        Vulkan::MemoryBuffer m_dummy_position_light_buffer;
+        VkWriteDescriptorSet m_position_light_write = {};
+        VkDescriptorBufferInfo m_position_light_buffer_info = {};
     };
 }
--- vulkan/physical_device.h
+++ vulkan/physical_device.h
@@ -12,6 +12,8 @@ namespace Vulkan {
 		static std::vector<VkQueueFamilyProperties> queueFamilyProperties;
 
 		static bool setMemoryType(uint32_t type_bits, VkFlags requirements_mask, uint32_t& type_index);
+        static uint32_t getAlignedMemorySize(const uint32_t size, const uint32_t count);
+        static uint32_t getAlignedMemoryStep(const uint32_t size);
 	};
 
 	VkResult CreatePhysicalDevice(PhysicalDevice& physical_device);
